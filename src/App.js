0b28d5ebf6795da773f01298602ab3d044386890
Sam Maaß <sam@maass.co>
2019-09-19 19:51:50 +0200

    make username dynamic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import React, { useState } from "react";
 import { GithubProfile } from "./GithubProfile";
-import React from "react";
 import "./App.css";
 import { githubService } from "./githubService";
 
 function App() {
+  const [userName, setUserName] = useState("");
+
+  const handleChange = e => setUserName(e.target.value);
+  const handleKeyUp = e => e.key === "Enter" && getUserInfo();
+  const getUserInfo = () => githubService.getUserInfo(userName);
+
   return (
     <div className="App">
-      <input type="text" name="userName" id="userName" />
-      <button onClick={githubService.getUserInfo}>Load</button>
+      <input
+        type="text"
+        name="userName"
+        id="userName"
+        value={userName}
+        onChange={handleChange}
+        onKeyUp={handleKeyUp}
+      />
+      <button onClick={getUserInfo}>Load</button>
       <GithubProfile />
     </div>
   );
--- src/githubService.js
+++ src/githubService.js
@@ -5,7 +5,7 @@ const userProfile = new Subject();
 export const githubService = {
   getUserInfo: async name => {
     userProfile.next({ isLoading: true });
-    const res = await fetch("https://api.github.com/users/sam-maass");
+    const res = await fetch("https://api.github.com/users/" + name);
     const user = await res.json();
     userProfile.next({ ...user });
   },
